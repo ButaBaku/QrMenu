b4d396732bd2f79cefd912732abfd01ab69e162c
Mahammad <mahammad.code@gmail.com>
2024-06-03 20:31:44 +0400

    baliq font size generator fixed

--- data/Baliq_yemekleri.js
+++ data/Baliq_yemekleri.js
@@ -130,7 +130,7 @@ baliq_yemekleri_avropa.map((item) => {
   <div class="col-12 col-md-6">
                 <div class="mehsul">
                   <div class="name-price">
-                    <p>${item.name} <span class="gram">${item.gram}</span></p>
+                    <p class="${func(item)}">${item.name} <span class="gram">${item.gram}</span></p>
                     <span>${item.price} AZN</span>
                   </div>
       
