69800c3b58ff5748cc69830aa1e5e8cad8d5e7c0
Baloghlan <qasimovbaloglan13@gmail.com>
2024-06-04 01:05:37 +0400

    usaq menyusu

--- data/Usaq.js
+++ data/Usaq.js
@@ -2,8 +2,10 @@ const usaq_yemekleri = [
     {
         id: 1,
         name: "Toyuq Naqqetsləri",
-        gram: "(Free ilə) (250qr)",
+        nameEng: "Chicken nuggets",
+        gram: "(free) (250qr)",
         desc: "",
+        descEng: "",
         price: 7.00
     },
     
@@ -11,64 +13,88 @@ const usaq_yemekleri = [
     {
         id: 2,
         name: "Kartof Free",
+        nameEng: "French fries",
         gram: "(150qr)",
         desc: "",
+        descEng: "",
         price: 3.00
     },
 
     {
         id: 3,
         name: "Burger / Toyuq",
-        gram: "(Ət / Toyuq) (200qr)",
-        desc: "",
-        price: "5.00 / 4.00"
+        nameEng: "Burger",
+        gram: "(200qr)",
+        desc: "(Ət / toyuq)",
+        descEng: "(meat/chicken)",
+        price: "10.00 / 8.00"
     },
     
     {
         id: 4,
+        name: "Mini Burger",
+        gram: "(250qr)",
+        desc: "(Ət / toyuq)",
+        descEng: "(meat/chicken)",
+        price: "6.00 / 5.00"
+    },
+
+    {
+        id: 5,
         name: "Pizza Margarita",
+        nameEng: "Pizza Margarita",
         gram: "(250qr)",
         desc: "",
+        descEng: "",
         price: 8.00
     },
     
     {
-        id: 5,
-        name: "Pizza Polo",
-        gram: "(340qr)",
-        desc: "",
-        price: 12.00
-    },
-    
-    {
         id: 6,
-        name: "Peperoni",
+        name: "Pizza Polo",
+        nameEng: "Pizza Polo",
         gram: "(340qr)",
         desc: "",
+        descEng: "",
         price: 12.00
     },
     
     {
         id: 7,
-        name: "Mix Pizza",
-        gram: "(390qr)",
+        name: "Peperoni",
+        nameEng: "Peperoni",
+        gram: "(340qr)",
         desc: "",
-        price: 14.00
-    },
-    
-    {
-        id: 8,
-        name: "BBQ Pizza",
-        gram: "(330qr)",
-        desc: "",
+        descEng: "",
         price: 12.00
     },
     
     {
+        id: 8,
+        name: "Mix Pizza",
+        nameEng: "Mix Pizza",
+        gram: "(390qr)",
+        desc: "",
+        descEng: "",
+        price: 14.00
+    },
+    {
         id: 9,
+        name: "BBQ Pizza",
+        nameEng: "BBQ Pizza",
+        gram: "(330qr)",
+        desc: "",
+        descEng: "",
+        price: 12.00
+    },
+    
+    {
+        id: 10,
         name: "Sendviç Toyuq",
+        nameEng : "Chicken sandwich",
         gram: "(200 qr)",
         desc: "",
+        descEng: "",
         price: 6.00
     },
     
