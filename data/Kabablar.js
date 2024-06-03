b3b4a40c1793a04d019c6d979ca83c3365d6a061
Mahammad <mahammad.code@gmail.com>
2024-06-03 17:13:02 +0400

    kabablar completed

--- data/Kabablar.js
+++ data/Kabablar.js
@@ -152,6 +152,172 @@ const kabablar =[
         desc: "",
         descEng: "",
         price: 9.00
-      }
+      },
+      {
+        id: 19,
+        name: "Lamb on a stick",
+        nameEng: "Quzu çöp şiş",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 12.00
+      },
+      {
+        id: 20,
+        name: "Kartof-Quyruq",
+        nameEng: "Potato with lamb tail",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 5.00
+      },
+      {
+        id: 21,
+        name: "Şapalaq",
+        nameEng: "Shapalaq",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 16.00
+      },
+      {
+        id: 22,
+        name: "Manqalda Dana Dili",
+        nameEng: "Grilled beef tongue",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 9.00
+      },
+      {
+        id: 23,
+        name: "Koroğlu Kababı",
+        nameEng: "Koroghlu kebab",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 15.00
+      },
+      {
+        id: 24,
+        name: "Hinduşka Kababı",
+        nameEng: "Turkey kebab",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 40.00
+      },
+      {
+        id: 25,
+        name: "Çolpa Setkada",
+        nameEng: "Chicken on a grid",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 15.00
+      },
+      {
+        id: 26,
+        name: "Toyuq Kababı",
+        nameEng: "Chicken kebab",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 6.00
+      },
+      {
+        id: 27,
+        name: "Toyuq Lüləsi",
+        nameEng: "Chicken Lula",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 6.00
+      },
+      {
+        id: 28,
+        name: "Toyuq Qanad",
+        nameEng: "Chicken Wings",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 6.00
+      },
+      {
+        id: 29,
+        name: "Türk Sayağı Qanad",
+        nameEng: "Turkish-style chicken wings",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 7.00
+      },
+      // qiymeti yoxdu
+      {
+        id: 30,
+        name: "Nərə Balığı Kababı",
+        nameEng: "Sturgeon kebab",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 0
+      },
+      {
+        id: 31,
+        name: "Tərəvəz Kababı",
+        nameEng: "Vegetable kebab",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 0.70
+      },
+      {
+        id: 32,
+        name: "Kartof Külləmə",
+        nameEng: "Ash-roasted potato kebab",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 0.50
+      },
+      {
+        id: 33,
+        name: "Kartof Lüləsi",
+        nameEng: "Potato Lula",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 2.50
+      },
+      {
+        id: 34,
+        name: "Can Əti Çöpdə",
+        nameEng: "Tenderloin on a stick",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 14.00
+      },
+      {
+        id: 35,
+        name: "Bildirçin Kababə",
+        nameEng: "Quail kebab",
+        gram: "",
+        desc: "",
+        descEng: "",
+        price: 6.00
+      },
 ]
+
+// 1 eded artiqdi say 34 --- 17->19
+// id : 30 -> qiymet yoxdu nere baliginda
+
+
+
+
+
+
+
+
+
 
