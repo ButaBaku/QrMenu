739ed74a8095a72c573eeefd302d3f4f7bf22537
Mahammad <mahammad.code@gmail.com>
2024-06-05 20:28:29 +0400

    language interface and service btn updated | plov fixed

--- js/script.js
+++ js/script.js
@@ -3,18 +3,58 @@ if(localStorage.getItem("langMode")==undefined){
 }
 
 
-const langBtn = document.querySelector(".langBtn");
+const serviceBtn = document.getElementById("service-fee-div");
+const langBtn = document.getElementById("langBtn");
 
 const langFunc = ()=>{
   if(localStorage.getItem("langMode")=="az"){
     localStorage.setItem("langMode","en");
+    langBtn.innerHTML="AZE";
   }else{
     localStorage.setItem("langMode","az");
+    langBtn.innerHTML="EN";
   }
   window.location.reload();
 }
 
+langBtn.innerHTML = localStorage.getItem("langMode") == "az" ? "EN" : "AZE";
 langBtn.onclick=langFunc;
+
+const azeServiceDivInner = 
+`
+<p>+10%</p>
+<p>S</p>
+<p>E</p>
+<p>R</p>
+<p>V</p>
+<p>İ</p>
+<p>S</p>
+<span class="my-3">
+  <p>H</p>
+  <p>A</p>
+  <p>Q</p>
+  <p>Q</p>
+  <p>I</p>
+</span>`;
+
+const engServiceDivInner =
+`
+<p>+10%</p>
+<p>S</p>
+<p>E</p>
+<p>R</p>
+<p>V</p>
+<p>İ</p>
+<p>C</p>
+<p>E</p>
+<span class="my-3">
+  <p>F</p>
+  <p>E</p>
+  <p>E</p>
+</span>
+`
+serviceBtn.innerHTML = localStorage.getItem("langMode") == "az" ? azeServiceDivInner : engServiceDivInner
+
 
 const func_price =(price)=>{
     if(price.length > 8){
