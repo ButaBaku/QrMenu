0f302eb93da10d7bc4fb5c1310b42faa81878a1f
Baloghlan <qasimovbaloglan13@gmail.com>
2024-06-06 13:22:31 +0400

    some translates from ui

--- js/script.js
+++ js/script.js
@@ -67,3 +67,50 @@ const func_price =(price)=>{
         return "long-word"
     }
   }
+
+
+
+
+  const milli_yemekler_lang = document.querySelector("#milli_yemekler_lang");
+  const xemir_yemekler_lang = document.querySelector("#xemir_yemekler_lang");
+  const tendir_yemekler_lang = document.querySelector("#tendir_yemekler_lang");
+  const steak_lang = document.querySelector("#steak_lang");
+  const isti_yemekler_lang = document.querySelector("#isti_yemekler_lang");
+  const soyuq_qelyanaltilar_lang = document.querySelector("#soyuq_qelyanaltilar_lang");
+  const isti_qelyanaltilar_lang = document.querySelector("#isti_qelyanaltilar_lang");
+  const sac_yemekleri_lang = document.querySelector("#sac_yemekleri_lang");
+  const plovlar_lang = document.querySelector("#plovlar_lang");
+
+
+
+  
+  plovlar_lang
+
+if (localStorage.getItem("langMode") == 'az') {
+  milli_yemekler_lang.textContent = 'Mİllİ YEMƏKLƏR';
+  xemir_yemekler_lang.textContent = 'XƏMİR YEMƏKLƏR';
+  tendir_yemekler_lang.textContent = 'TƏNDİR YEMƏKLƏR';
+  steak_lang.textContent = 'İSTİ YEMƏKLƏR';
+  isti_yemekler_lang.textContent = 'STEAK';
+  soyuq_qelyanaltilar_lang.textContent = 'SOYUQ QƏLYANALTILAR';
+  isti_qelyanaltilar_lang.textContent = 'İSTİ QƏLYANALTILAR';
+  sac_yemekleri_lang.textContent = 'SAC YEMƏKLƏR';
+  plovlar_lang.textContent = 'plovlar';
+
+
+
+  
+} else {
+  milli_yemekler_lang.textContent = 'NATIONAL DISHES';
+  xemir_yemekler_lang.textContent = 'FARINACEOUS DISHES';
+  tendir_yemekler_lang.textContent = 'TANDOORI DISHES';
+  steak_lang.textContent = 'STEAK';
+  isti_yemekler_lang.textContent = 'HOT DISHER';
+  soyuq_qelyanaltilar_lang.textContent = 'COLD SNACKS';
+  isti_qelyanaltilar_lang.textContent = 'HOT SNACKS';
+  sac_yemekleri_lang.textContent = 'SAJ DISHES';
+  plovlar_lang.textContent = 'PILAFS';
+
+
+
+}
