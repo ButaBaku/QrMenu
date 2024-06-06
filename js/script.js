03ff20f18368cef61608a3fcf6584e9a035870b2
Baloghlan <qasimovbaloglan13@gmail.com>
2024-06-06 13:46:23 +0400

    index buttons lang ready

--- js/script.js
+++ js/script.js
@@ -80,11 +80,28 @@ const func_price =(price)=>{
   const isti_qelyanaltilar_lang = document.querySelector("#isti_qelyanaltilar_lang");
   const sac_yemekleri_lang = document.querySelector("#sac_yemekleri_lang");
   const plovlar_lang = document.querySelector("#plovlar_lang");
+  const baliq_yemekleri_lang = document.querySelector("#baliq_yemekleri_lang");
+  const toyuq_yemekleri_lang = document.querySelector("#toyuq_yemekleri_lang");
+  const burgerler_lang = document.querySelector("#burgerler_lang");
+  const rollsandvich_lang = document.querySelector("#rollsandvich_lang");
+  const pizzalar_lang = document.querySelector("#pizzalar_lang");
+  const usaq_menyusu_lang = document.querySelector("#usaq_menyusu_lang");
+  const qarnirler_lang = document.querySelector("#qarnirler_lang");
+  const sorbalar_lang = document.querySelector("#sorbalar_lang");
+  const pastalar_lang = document.querySelector("#pastalar_lang");
+  const salatlar_lang = document.querySelector("#salatlar_lang");
+  const serin_ickiler_lang = document.querySelector("#serin_ickiler_lang");
+  const piveler_lang = document.querySelector("#piveler_lang");
+  const pive_mezeleri_lang = document.querySelector("#pive_mezeleri_lang");
+  const isti_ickiler_lang = document.querySelector("#isti_ickiler_lang");
+  const desertler_lang = document.querySelector("#desertler_lang");
+  const seher_yemek_setleri_lang = document.querySelector("#seher_yemek_setleri_lang");
+  const qelyan_setleri_lang = document.querySelector("#qelyan_setleri_lang");
+  const shisha_setleri_lang = document.querySelector("#shisha_setleri_lang");
 
 
 
-  
-  plovlar_lang
+  shisha_setleri_lang
 
 if (localStorage.getItem("langMode") == 'az') {
   milli_yemekler_lang.textContent = 'Mİllİ YEMƏKLƏR';
@@ -96,6 +113,24 @@ if (localStorage.getItem("langMode") == 'az') {
   isti_qelyanaltilar_lang.textContent = 'İSTİ QƏLYANALTILAR';
   sac_yemekleri_lang.textContent = 'SAC YEMƏKLƏR';
   plovlar_lang.textContent = 'plovlar';
+  baliq_yemekleri_lang.textContent = 'Balıq YEMƏKLƏR';
+  toyuq_yemekleri_lang.textContent = 'TOYUQ YEMƏKLƏR';
+  burgerler_lang.textContent = 'BURGERLƏR';
+  rollsandvich_lang.textContent = 'ROLL VƏ SENDVİÇ';
+  pizzalar_lang.textContent = 'PİZZALAR';
+  usaq_menyusu_lang.textContent = 'UŞAQ MENYUSU';
+  qarnirler_lang.textContent = 'QARNİRLƏR';
+  sorbalar_lang.textContent = 'ŞORBALAR';
+  pastalar_lang.textContent = 'PASTALAR';
+  salatlar_lang.textContent = 'SALATLAR';
+  serin_ickiler_lang.textContent = 'SƏRİN İÇKİLƏR';
+  piveler_lang.textContent = 'PİVƏLƏR';
+  pive_mezeleri_lang.textContent = 'PİVƏ MƏZƏLƏRİ';
+  isti_ickiler_lang.textContent = 'İSTİ İÇKİLƏR';
+  desertler_lang.textContent = 'DESERTLƏR';
+  seher_yemek_setleri_lang.textContent = 'SƏHƏR YEMƏK SETLƏRİ';
+  qelyan_setleri_lang.textContent = 'QƏLYAN SETLƏRİ';
+  shisha_setleri_lang.textContent = 'SHİSHA SETS';
 
 
 
@@ -109,7 +144,26 @@ if (localStorage.getItem("langMode") == 'az') {
   soyuq_qelyanaltilar_lang.textContent = 'COLD SNACKS';
   isti_qelyanaltilar_lang.textContent = 'HOT SNACKS';
   sac_yemekleri_lang.textContent = 'SAJ DISHES';
-  plovlar_lang.textContent = 'PILAFS';
+  plovlar_lang.textContent = 'plovlar';
+  baliq_yemekleri_lang.textContent = 'FISH DISHES';  
+  toyuq_yemekleri_lang.textContent = 'TOYUQ YEMƏKLƏR';
+  burgerler_lang.textContent = 'BURGERS';  
+  rollsandvich_lang.textContent = 'ROLL AND SANDWICH';  
+  pizzalar_lang.textContent = 'PİZZAS';  
+  usaq_menyusu_lang.textContent = 'KIDS MENU';  
+  qarnirler_lang.textContent = 'GARNİSHE';  
+  sorbalar_lang.textContent = 'SOUPS';  
+  pastalar_lang.textContent = 'PASTALAR';  
+  salatlar_lang.textContent = 'SALADS';  
+  serin_ickiler_lang.textContent = 'COLD DRINKS';  
+  piveler_lang.textContent = 'BEERS';  
+  pive_mezeleri_lang.textContent = 'BEER APPETİZERS';  
+  isti_ickiler_lang.textContent = 'HOT DRINKS';  
+  desertler_lang.textContent = 'DESSERTS';  
+  seher_yemek_setleri_lang.textContent = 'BREAKFAST SETS';  
+  qelyan_setleri_lang.textContent = 'SHİSHA SETS';  
+  shisha_setleri_lang.textContent = 'OUR SHİSHA SETS';  
+  
 
 
 
