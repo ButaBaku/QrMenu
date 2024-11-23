253f46d2ee6a719e8c6a773cfaa64bd4e31edf07
Baloghlan <qasimovbaloglan13@gmail.com>
2024-11-23 22:47:20 +0400

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,40 +2,42 @@ import React from 'react'
 import { FaFacebookF } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
+import { FaPhoneSquareAlt } from "react-icons/fa";
 const Footer = () => {
   return (
+    <div className="bg-brown">
+    <div className="container">
     <div className="footer">
         <h4 className="footer-name">Buta Baku Restaurant</h4>
         
-        <div className="socials row">
+        <div className="socials">
         
-        <div className="col-3 col-md-12">
+        <div className="row">
+        <div className="col-6 col-md-3">
         <div className="social-card">
-                <div className="icon"><FaFacebookF /></div>
-                <p>+994123456789</p>
-            </div>
-            </div>
-            <div className="col-3 col-md-12">
-        <div className="social-card">
-                <div className="icon"><FaFacebookF /></div>
-                <p>+994123456789</p>
-            </div>
-            </div>
-            {/* <div className="col-3 col-md-12"> */}
-        {/* <div className="social-card">
-                <div className="icon"><FaFacebookF /></div>
-                <p>+994123456789</p>
-            </div>
-            </div>
-            <div className="social-card">
                 <div className="icon"><FaPhoneAlt /></div>
-                <p>+994123456789</p>
+                <p className="social-text">+994123456789</p>
             </div>
+            </div>
+            <div className="col-6 col-md-3">
             <div className="social-card">
                 <div className="icon"><FaInstagram  /></div>
-                <p>+994123456789</p>
-            </div> */}
+                <a href="" className="social-text">Instagram</a>
+            </div> 
+            </div>
+            </div>
         </div>
+        <div className="location">
+                <h5>Ünvan</h5>
+                <p>Filan filan yer</p>
+            </div>
+
+        <div className="last-footer">
+        © Buta Baku
+        </div>
+
+    </div>
+    </div>
     </div>
   )
 }
