80e676e4108f3128e68630ee53e8ed488de4191a
Baloghlan <qasimovbaloglan13@gmail.com>
2024-11-23 21:48:25 +0400

    added foot-card design

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './assets/sass/style.scss'
+import FoodCard from './components/FoodCard'
+import Foods from './components/Foods'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Foods headerTitle="Səhər Yeməyi"/>
+    </div>
   )
 }
 
---
+++ src/components/FoodCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FoodCard = ({img,name,price,desc}) => {
+  return (
+    <div className="col-12 col-md-4">
+<div className="card food-card">
+  <img src={img} className="card-img-top food-img" alt="..." />
+  <div className="card-body food-card-body">
+    <div className="head-body">
+    <h5 className="card-title card-text food-card-name">{name}</h5>
+    <h5 className="card-title card-text food-card-price">{price}azn</h5>
+    </div>
+
+    <p className="card-text food-card-desc">{desc}</p>
+  </div>
+</div>
+    </div>
+    
+  )
+}
+
+export default FoodCard
